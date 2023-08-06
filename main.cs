0bb384cf5ed3bcddeba6f779b26b4908753b585d
LAU Kwan Kit <53417646+lofung@users.noreply.github.com>
2023-08-06 19:30:55 -0400

    database connection done

--- main.cs
+++ main.cs
@@ -12,7 +12,7 @@ namespace HelloWorldApp {
               
             // statement
             // printing Hello World!
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Database access has already been done on April 14, 2023 by LAU KK");
               
             // To prevents the screen from 
             // running and closing quickly
